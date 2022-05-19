8c87f819f5dc5d581db5b21c115e8a17e112c567
boris <boris91a@gmail.com>
2022-05-19 23:41:44 +0300

    Posts loading from data file

--- posts.php
+++ posts.php
@@ -1,36 +1,31 @@
 <?php
 $title = "Posts";
 $active['posts'] = " active";
+$posts = unserialize(file_get_contents("posts.txt"));
 $content = <<<HEREDOC
     <div class="container">
         <div class="row">
             <div class="col-12"><h2 class="underline">$title</h2></div>
         </div>
         <div class="row mt-4 justify-content-center">
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="post">
-                    <div id="public-date">19.05.2022 21:01</div>
-                    <div class="post-content">
-                        <img src="images/post1.jpg" alt="Post pic">
-                        <h4 class="mt-2">Some post</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores commodi culpa, cumque dolorum expedita fugit libero molestiae odio quasi quia rem reprehenderit repudiandae sed suscipit? Error fugit impedit perspiciatis!</p>
-                    </div>
-                    <div class="open-post"><a href="#">Перейти>>></a></div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="post">
-                    <div id="public-date">19.05.2022 21:01</div>
-                    <div class="post-content">
-                        <img src="images/post2.jpg" alt="Post pic">
-                        <h4 class="mt-2">Second post</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet consectetur deserunt eum explicabo fuga itaque, labore magnam molestias nostrum odit officia praesentium quaerat quia quo ratione sapiente sed soluta.</p>
-                    </div>
-                    <div class="open-post"><a href="#">Перейти>>></a></div>
-                </div>
-            </div>
-        </div>
-    </div>
 HEREDOC;
+foreach($posts as $post):
+    extract($post);
+    $content .= <<<HEREDOC
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="post">
+                    <div id="public-date">$date</div>
+                    <div class="post-content">
+                        <img src="images/$picture" alt="Post pic">
+                        <h4 class="mt-2">$header</h4>
+                        <p>$p_content</p>
+                    </div>
+                    <div class="open-post"><a href="#">Перейти>>></a></div>
+                </div>
+            </div>
+HEREDOC;
+endforeach;
+$content .="</div></div>";
+
 require_once "layout/main.php";
 ?>
